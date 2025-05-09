20b4e47cb7d0bf20a69cc93610b4fb9ad198406f
Raheel <noor144mroman@gmail.com>
2025-05-09 16:46:33 +0400

    update4

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://library-app-gtyn.onrender.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 dotenv.config();
 
 const constr = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@personallibrary.zf5mldr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=PersonalLibrary`;
